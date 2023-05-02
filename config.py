f7eb61b6eb85063218a0ea7a2936a2569109bba0
Sergey Kim <sergkim7@gmail.com>
2023-05-02 17:01:20 +0300

    upd CI yaml

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ HASH_SCHEMA = os.environ.get('HASH_SCHEMA', 'bcrypt')
 
 # Database
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
+DB_PORT = os.environ.get('DB_PORT', 5432)
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
